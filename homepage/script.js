6958d6b622841b1f8db1789bbd72d5e2c0a204bb
pbbblo <pablobae2003@gmail.com>
2022-02-05 23:19:58 -0500

    modal

--- homepage/script.js
+++ homepage/script.js
@@ -1,1 +1,9 @@
 console.log("App JS connected.");
+
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 47.7848201936765, lng: -87.40461005289102},
+        zoom: 6,
+        mapId: '58acd8972fb835b2'
+        });
+}
