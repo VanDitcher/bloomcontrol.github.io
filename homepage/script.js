cb4de9bce11d14529a1669a713e216b97365e05e
pbbblo <pablobae2003@gmail.com>
2022-02-05 23:42:00 -0500

    kdsak,dsak,dk,as

--- homepage/script.js
+++ homepage/script.js
@@ -7,3 +7,15 @@ function initMap(){
         mapId: '58acd8972fb835b2'
         });
 }
+
+const open = document.getElementsById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+})
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+})
