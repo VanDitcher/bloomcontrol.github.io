f07da36ed14d51b36661d689cd5dc1de6481e343
Eldar Zulic <eldarzulich03@gmail.com>
2022-02-05 23:08:37 -0500

    Added map files

---
+++ Ehacks/script.js
@@ -0,0 +1,7 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 47.7848201936765, lng: -87.40461005289102},
+        zoom: 6,
+        mapId: '58acd8972fb835b2'
+        });
+}
